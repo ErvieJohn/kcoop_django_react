0ed26df341dfff9003d1f60f9138487ae1ca12a8
ErvieJohn <erviejohn.villareal79@gmail.com>
2023-07-04 17:00:54 +0800

    upload image in cms

--- kcoop_react/src/screens/CMS/screens/CMSHome.js
+++ kcoop_react/src/screens/CMS/screens/CMSHome.js
@@ -1,10 +1,19 @@
 import {React, useState} from 'react';
 
+
 const CMSHome = () => {
+  const [image, setImage] = useState('');
   
+  function handleImage(e){
+    console.log(e.target.files);
+    setImage(e.target.files[0]);
+    
+  }
 
   return (
-    <div> <p>HOMEEEEEEEEEEEEE</p></div>
+    <div> <p>HOMEEEEEEEEEEEEE</p>
+      <input type="file" name="file" accept='image/*' onChange={handleImage}/>
+    </div>
   )
 }
 export default CMSHome;
