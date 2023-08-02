11773bbe97835e61bc7b55d92e8324a7321e0588
ErvieJohn <erviejohn.villareal79@gmail.com>
2023-08-02 14:30:22 +0800

    added auto logout when inactive user

--- kcoop_react/src/screens/CMS/Dashboard.js
+++ kcoop_react/src/screens/CMS/Dashboard.js
@@ -6,6 +6,15 @@ import Navbar from './Navbar/Navbar';
 
 import './Dashboard.css';
 
+const events = [
+  "load",
+  "mousemove",
+  "mousedown",
+  "click",
+  "scroll",
+  "keypress",
+];
+
 function Dashboard() {
   const [User, setUser] = useState(localStorage.getItem('USER'));
   const [isOpen, setOpen] = useState(false);
@@ -40,6 +49,39 @@ function Dashboard() {
     //localStorage.removeItem('isDarkMode');
     localStorage.setItem('isDarkMode', !darkMode);
   }
+
+  let timer;
+
+  // this function sets the timer that logs out the user after 10 secs
+  const handleLogoutTimer = () => {
+    timer = setTimeout(() => {
+      // clears any pending timer.
+      resetTimer();
+      // Listener clean up. Removes the existing event listener from the window
+      Object.values(events).forEach((item) => {
+        window.removeEventListener(item, resetTimer);
+      });
+      // logs out user
+      AuthLogout();
+    }, 300000); // 300000ms = 5mins. You can change the time.
+  };
+
+  // this resets the timer if it exists.
+  const resetTimer = () => {
+    if (timer) clearTimeout(timer);
+  };
+
+  // when component mounts, it adds an event listeners to the window
+  // each time any of the event is triggered, i.e on mouse move, click, scroll, keypress etc, the timer to logout user after 10 secs of inactivity resets.
+  // However, if none of the event is triggered within 10 secs, that is app is inactive, the app automatically logs out.
+  useEffect(() => {
+    Object.values(events).forEach((item) => {
+      window.addEventListener(item, () => {
+        resetTimer();
+        handleLogoutTimer();
+      });
+    });
+  }, []);
 
   return(
     <>
