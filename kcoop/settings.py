07fdda581263eb2dea3217337999a4109748e9eb
ErvieJohn <erviejohn.villareal79@gmail.com>
2023-10-26 10:53:52 +0800

    10_26_23 fixed the footer

--- kcoop/settings.py
+++ kcoop/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware', #for whitenoise
+    'whitenoise.middleware.WhiteNoiseMiddleware', #for whitenoise
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -146,12 +146,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'django_react_static', 'static')
 
 # without whitenoise
 #MEDIA_URL = '/static/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'kcoop_react/build/static/media') # kcoop_react/build/static/media # main
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'kcoop_react/build/static/media') # kcoop_react/build/static/media # main
 
 # with whitenoise # uncomment whitenoise in in MIDDLEWARE
 MEDIA_URL = '/static/media/'
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'django_react_static', 'static', 'media')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'django_react_static', 'static', 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- kcoop_react/src/config.js
+++ kcoop_react/src/config.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 //export var BASE_URL = 'http://127.0.0.1:8000'; // For Localhost
 //export var BASE_URL = 'http://192.168.0.101:82'; // For localhost port 82
-//export var BASE_URL = 'http://103.3.61.162:82'; // For Deployment
+export var BASE_URL = 'http://103.3.61.162:82'; // For Deployment
 
-export var BASE_URL = 'http://192.168.0.105:8000'; // For LocalIP
+//export var BASE_URL = 'http://192.168.0.105:8000'; // For LocalIP
 
 //export var BASE_URL = 'https://0d83-112-199-37-99.ngrok-free.app'; // Domain
